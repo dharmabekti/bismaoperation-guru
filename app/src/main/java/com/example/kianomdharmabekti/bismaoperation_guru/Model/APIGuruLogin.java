69c4792d7a1854314ab4208a29b78465fbf73873
mery pamungkas <mery pamungkas>
2017-05-25 02:03:39 +0700

    login jadi

---
+++ app/src/main/java/com/example/kianomdharmabekti/bismaoperation_guru/Model/APIGuruLogin.java
@@ -0,0 +1,209 @@
+package com.example.kianomdharmabekti.bismaoperation_guru.Model;
+
+/**
+ * Created by mery on 5/25/2017.
+ */
+public class APIGuruLogin {
+    private boolean status;
+    private int status_code;
+    private String message;
+    private ResponBean respon;
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+    public int getStatus_code() {
+        return status_code;
+    }
+
+    public void setStatus_code(int status_code) {
+        this.status_code = status_code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public ResponBean getRespon() {
+        return respon;
+    }
+
+    public void setRespon(ResponBean respon) {
+        this.respon = respon;
+    }
+
+    public static class ResponBean {
+        /**
+         * id : 5
+         * nama_depan : Dharma
+         * nama_belakang : Bekti
+         * tempat_lahir : Purworejo
+         * alamat : Jrakah
+         * telepon : 085740126916
+         * kelamin : Pria
+         * tanggal_lahir : 1996-06-16
+         * status : On
+         * email : dharmabekti@gmail.com
+         * longitude : null
+         * latitude : null
+         * harga : 50000
+         * billing : 0
+         * riwayat_pendidikan : SMA
+         * mata_pelajaran : Matematika SD, Matematika SMP, Matematika SMA
+         */
+
+        private int id;
+        private String nama_depan;
+        private String nama_belakang;
+        private String tempat_lahir;
+        private String alamat;
+        private String telepon;
+        private String kelamin;
+        private String tanggal_lahir;
+        private String status;
+        private String email;
+        private Object longitude;
+        private Object latitude;
+        private String harga;
+        private String billing;
+        private String riwayat_pendidikan;
+        private String mata_pelajaran;
+
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        public String getNama_depan() {
+            return nama_depan;
+        }
+
+        public void setNama_depan(String nama_depan) {
+            this.nama_depan = nama_depan;
+        }
+
+        public String getNama_belakang() {
+            return nama_belakang;
+        }
+
+        public void setNama_belakang(String nama_belakang) {
+            this.nama_belakang = nama_belakang;
+        }
+
+        public String getTempat_lahir() {
+            return tempat_lahir;
+        }
+
+        public void setTempat_lahir(String tempat_lahir) {
+            this.tempat_lahir = tempat_lahir;
+        }
+
+        public String getAlamat() {
+            return alamat;
+        }
+
+        public void setAlamat(String alamat) {
+            this.alamat = alamat;
+        }
+
+        public String getTelepon() {
+            return telepon;
+        }
+
+        public void setTelepon(String telepon) {
+            this.telepon = telepon;
+        }
+
+        public String getKelamin() {
+            return kelamin;
+        }
+
+        public void setKelamin(String kelamin) {
+            this.kelamin = kelamin;
+        }
+
+        public String getTanggal_lahir() {
+            return tanggal_lahir;
+        }
+
+        public void setTanggal_lahir(String tanggal_lahir) {
+            this.tanggal_lahir = tanggal_lahir;
+        }
+
+        public String getStatus() {
+            return status;
+        }
+
+        public void setStatus(String status) {
+            this.status = status;
+        }
+
+        public String getEmail() {
+            return email;
+        }
+
+        public void setEmail(String email) {
+            this.email = email;
+        }
+
+        public Object getLongitude() {
+            return longitude;
+        }
+
+        public void setLongitude(Object longitude) {
+            this.longitude = longitude;
+        }
+
+        public Object getLatitude() {
+            return latitude;
+        }
+
+        public void setLatitude(Object latitude) {
+            this.latitude = latitude;
+        }
+
+        public String getHarga() {
+            return harga;
+        }
+
+        public void setHarga(String harga) {
+            this.harga = harga;
+        }
+
+        public String getBilling() {
+            return billing;
+        }
+
+        public void setBilling(String billing) {
+            this.billing = billing;
+        }
+
+        public String getRiwayat_pendidikan() {
+            return riwayat_pendidikan;
+        }
+
+        public void setRiwayat_pendidikan(String riwayat_pendidikan) {
+            this.riwayat_pendidikan = riwayat_pendidikan;
+        }
+
+        public String getMata_pelajaran() {
+            return mata_pelajaran;
+        }
+
+        public void setMata_pelajaran(String mata_pelajaran) {
+            this.mata_pelajaran = mata_pelajaran;
+        }
+    }
+}
--- app/src/main/java/com/example/kianomdharmabekti/bismaoperation_guru/REST/RestClient.java
+++ app/src/main/java/com/example/kianomdharmabekti/bismaoperation_guru/REST/RestClient.java
@@ -1,6 +1,7 @@
 package com.example.kianomdharmabekti.bismaoperation_guru.REST;
 
 import com.example.kianomdharmabekti.bismaoperation_guru.HELPER.ToStringConverter;
+import com.example.kianomdharmabekti.bismaoperation_guru.Model.APIGuruLogin;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.squareup.okhttp.Interceptor;
@@ -9,6 +10,7 @@ import com.squareup.okhttp.Response;
 
 import java.io.IOException;
 
+import retrofit.Call;
 import retrofit.GsonConverterFactory;
 import retrofit.Retrofit;
 import retrofit.http.Field;
@@ -23,8 +25,8 @@ import retrofit.http.POST;
 public class RestClient {
     private static GitApiInterface gitApiInterface;
     //    private static String baseUrl = "http://bisma.bluecrawler.com" ;
-    private static String baseUrl = "http://api.bismaoperation.id" ;
-    private static final String token = "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOi8vd3d3LmFwaS5iaXNtYW9wZXJhdGlvbi5pZC9wdWJsaWMvYXV0aC9sb2dpbiIsImlhdCI6MTQ5NTQ2MzY3MywiZXhwIjoxNDk1NTIzNjczLCJuYmYiOjE0OTU0NjM2NzMsImp0aSI6IkNHclVOUFdNY3Q2TXprZDYiLCJzdWIiOjZ9.7bVkzdxaZlrclX1FgXswtNrwOoHgpxQH7LSDBDNm7GE";
+    private static String baseUrl = "http://bismaapi.bismaoperation.id" ;
+    private static final String token = "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOi8vYmlzbWFhcGkuYmlzbWFvcGVyYXRpb24uaWQvcHVibGljL2F1dGgvbG9naW4iLCJpYXQiOjE0OTU2MzgxMjIsImV4cCI6MTUyNjc0MjEyMiwibmJmIjoxNDk1NjM4MTIyLCJqdGkiOiJSTFdrR0h2aElpemZuSnRmIiwic3ViIjoyMH0.hoeTI_g9YalUHGKbD0o7ABzNGEGn6aHzTaauMG00Lws";
 
 
     public static GitApiInterface getClient(){
@@ -57,8 +59,44 @@ public class RestClient {
 
         @FormUrlEncoded
         @Headers("Authorization: "+token)
-        @POST("/public/Murid/LoginMurid")
-        Call<APIMuridData> login(@Field("username") String username, @Field("password") String password);
+        @POST("/public/Pengajar/LoginPengajar")
+        Call<APIGuruLogin> login(@Field("username") String username, @Field("password") String password);
 
+       /* @FormUrlEncoded
+        @Headers("Authorization: "+token)
+        @POST("/public/Murid/RegisterMurid")
+        Call<APIBaseResponse> signUp(@Field("nama_depan") String nama_depan, @Field("nama_belakang") String nama_belakang, @Field("alamat") String alamat, @Field("tempat_lahir") String tempat_lahir ,
+                                     @Field("telepon")String nomor_telepon, @Field("kelamin") String jenis_kelamin, @Field("tanggal_lahir") String birthDate,
+                                     @Field("email") String email, @Field("username") String username,
+                                     @Field("password") String password, @Field("longitude") String longitude, @Field("latitude") String latitude);
+
+
+        @Headers("Authorization: "+token)
+        @GET("public/Pengajar/ListPengajar")
+        Call<APIGuruData> showguru();
+
+  //      @FormUrlEncoded
+        //@Headers("Authorization: "+token)
+      //  @POST("/public/Order/TambahOrder")
+    //    Call<APITambahOrder> order(@Field("id_pengajar") String idpengajar, @Field("id_murid") String idmurid);
+
+//        @Headers("Authorization: "+token)
+//        @GET("/public/Order/ListPengajar")
+//        Call<APIOrderData> orderdetil(@Query("id_murid")int idmurid);
+
+       /* @Headers("Authorization: "+token)
+        @GET("/public/Order/ListPengajar")
+        Call<APIOrderListPengajar> orderdetil(@Query("id_murid") int idmurid);
+
+        @FormUrlEncoded
+        @Headers("Authorization: "+token)
+        @POST("public/Order/Pembayaran")
+        Call<APIBayar> bayar(@Field("id_order") String idorder);
+
+        @FormUrlEncoded
+        @Headers("Authorization: "+token)
+        @POST("public/Order/Deal")
+        Call<APIDealOrder> dealorder(@Field("id_order") String idorder);
+*/
     }
 }
